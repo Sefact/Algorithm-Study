bbe4e16b7a184f5b02d5465893ce2530245c4671
Hyoyeon Kim <Sefact@users.noreply.github.com>
2023-05-13 16:51:27 +0900

    [level 2] Title: 모음 사전, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/84512. 모음 사전/모음 사전.py
@@ -0,0 +1,14 @@
+# A > AA > AAA > AAAA > AAAAA ~ UUUUU
+
+from itertools import product
+
+def solution(word):
+    answer = 0
+    dic = { "A":0, "E":1, "I":2, "O":3, "U":4 }
+    
+    for i in range(len(word)):
+        for j in range(4, i, -1):
+            answer += 5 ** (j - i) * dic[word[i]]
+        answer += 1 + dic[word[i]]
+    
+    return answer
